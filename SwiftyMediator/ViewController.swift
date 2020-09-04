f605181fcf173f258eee76958422e0b336f09d5d
yingkelei <yingkelei@pinduoduo.com>
2020-09-04 11:14:23 +0800

    Initial Commit

---
+++ SwiftyMediator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyMediator
+//
+//  Created by yingkelei on 2020/9/4.
+//  Copyright © 2020 lilikazine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
